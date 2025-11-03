3f3cbb64a6952e67891469fb8d1945549b822b98
Athanase <ngaiporoathanase@gmail.com>
2025-11-03 16:43:08 +0100

    Mise a jour

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './App.css'
-import Interface from './Dribble/Interface'
+import Interface from './Interface/Interface'
 
 
 createRoot(document.getElementById('root')!).render(
